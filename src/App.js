f6c0d39efda667085dd415fe0e1de8100af1c23a
Deon Joshy <deonjoshy@gmail.com>
2022-07-28 12:06:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar/Navbar'
+import User from './components/user/User'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <User/>
+    </div>
+  );
+}
+
+export default App;
